72d6801264b974ef29851b2fbd7f7d4cb8670ef1
Justin Phan <jfmphan@gmail.com>
2013-02-26 18:31:40 -0800

    Merged FCFS and Proirity.

--- CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
+++ CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -2,7 +2,7 @@
  * 
  * A first-come first-served scheduling algorithm.
  *
- * @author: Kyle Benson
+ * @author: Mannchuoy Yam(16461232)
  * Winter 2013
  *
  */
@@ -12,23 +12,34 @@ import java.util.*;
 
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
-    FCFSSchedulingAlgorithm(){
-
+    private Vector<Process> jobs;
+    
+	FCFSSchedulingAlgorithm(){
+		activeJob = null;
+		jobs = new Vector<Process>(); 	
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-
+    	jobs.add(p);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
-    	return true;
+    	
+    	return jobs.remove(p);
+
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
 	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+    	for (int i = jobs.size()-1; i >= 0; i--) {
+    	    Process job = this.jobs.get(0);
+    	    this.removeJob(job);
+    	    otherAlg.addJob(job);
+    	}
+    	
     }
 
 
@@ -38,10 +49,13 @@ public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
     /** Returns the next process that should be run by the CPU, null if none available.*/
     public Process getNextJob(long currentTime){
-    	return null;
+
+    	activeJob = jobs.firstElement();
+    	return activeJob;
+
     }
 
     public String getName(){
-	return "First-come first-served";
+    	return "First-come first-served";
     }
 }
