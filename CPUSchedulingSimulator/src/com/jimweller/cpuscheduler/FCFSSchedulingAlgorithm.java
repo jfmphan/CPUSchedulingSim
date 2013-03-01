747e50b878f39b8751805ca80ece9a2a3b162b81
Justin Phan <jfmphan@gmail.com>
2013-03-01 15:16:06 -0800

    Working All Except RR

--- CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
+++ CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -10,34 +10,37 @@ package com.jimweller.cpuscheduler;
 
 import java.util.*;
 
+
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
-    private Vector<Process> jobs;
+    private PriorityQueue<Process> pqueue;
     
 	FCFSSchedulingAlgorithm(){
 		activeJob = null;
-		jobs = new Vector<Process>(); 	
+		Comparator<Process> comp = new FCComparator();
+    	pqueue = new PriorityQueue<Process>(50, comp);
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-    	jobs.add(p);
+    	pqueue.add(p);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
     	
-    	return jobs.remove(p);
+    	return pqueue.remove(p);
 
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
 	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
-    	for (int i = jobs.size()-1; i >= 0; i--) {
-    	    Process job = this.jobs.get(0);
-    	    this.removeJob(job);
-    	    otherAlg.addJob(job);
+    	
+    	while(!pqueue.isEmpty())
+    	{
+    		otherAlg.addJob(pqueue.peek());
+    		pqueue.remove();
     	}
     	
     }
@@ -50,7 +53,7 @@ public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
     /** Returns the next process that should be run by the CPU, null if none available.*/
     public Process getNextJob(long currentTime){
 
-    	activeJob = jobs.firstElement();
+    	activeJob = pqueue.peek();
     	return activeJob;
 
     }
@@ -58,4 +61,30 @@ public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
     public String getName(){
     	return "First-come first-served";
     }
+    
+    private class FCComparator implements Comparator<Process>
+    {
+    	public int compare(Process p1, Process p2) 
+		{
+			
+			int comp = 0;
+			
+			if(p1 != null && p2 != null)
+			{
+				if(p1.getArrivalTime() < p2.getArrivalTime())
+				{
+					comp = -1;
+				}
+				
+				if(p1.getArrivalTime() > p2.getArrivalTime())
+				{
+					comp = 1;
+				}
+			
+			}
+		
+			return comp;
+		}
+    }
+
 }
